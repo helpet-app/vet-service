c6c70283833872f3d45d71538c42d12ce2af90c3
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-07 23:02:09 +0700

    Init project

---
+++ src/main/java/com/helpet/service/vet/VetServiceApplication.java
@@ -0,0 +1,11 @@
+package com.helpet.service.vet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VetServiceApplication.class, args);
+    }
+}
